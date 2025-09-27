067eaaee538007c0da1a64a3b8cbf821b8da30e6
Julio Fernandez <juliofernanadezs35@gmail.com>
2025-09-27 12:38:53 -0300

    Clase 3

--- CLASE4/Lista.py
+++ CLASE4/Lista.py
@@ -6,3 +6,6 @@ array=["futbol","Pc",18.6,18,[6,7,10.5],True,False]
 print(array)
 #acceso a un elemento de la lista
 print(array[0])
+print("PC" in array)
+print(array.index("futbol")) #Devulve el indice del elemento
+print(array.cout("PC")) #Devuelve la cantidad de veces que se repite el elemento
